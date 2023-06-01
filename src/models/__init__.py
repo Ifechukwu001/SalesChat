0a0a3f69b8c38f9dd21bbc7b70f5f7eb9cc3683d
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-01 08:35:34 +0100

    Initialize dbengine object.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,4 +3,12 @@
 """
 from os import getenv
 
+
 storage_type = getenv("SC_STORAGE")
+storage = None
+
+if storage_type == "db":
+    from .engine.db_engine import DBEngine
+    storage = DBEngine()
+    storage.reload()
+
