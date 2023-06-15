9c035bdef0f07717ad8d95d03d4c3dc57dd6f2fe
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-06-15 16:13:35 +0100

    Implemented create product of the whatsapp

--- src/models/whatsapp.py
+++ src/models/whatsapp.py
@@ -36,6 +36,11 @@ class WhatsAppSender:
                         WhatsAppSender.bank_details(phone_number)
                     elif body.lower().startswith("update"):
                         WhatsAppSender.update_user(body, phone_number)
+                elif message_type == "image":
+                    caption = message[0]["image"]["caption"]
+                    image_id = message[0]["image"]["id"]
+                    if caption.lower().startswith("product"):
+                        WhatsAppSender.create_product(image_id, caption, phone_number)
                     
     @classmethod
     def hello(cls, phone_number: str):
@@ -77,11 +82,11 @@ class WhatsAppSender:
                       "name: \n" \
                       "description: \n" \
                       "price: \n" \
-                      "category: (good/service/digital)\n"\
+                      "category: (goods/service/digital)\n"\
                       "quantity: "
                 WhatsAppSender.message(msg, phone_number)
-                msg = "Ps: Leave quantity blank if product " \
-                      "is digital. Select only one category"
+                msg = "Ps: Price is in NGN, "\
+                      "description & quantity (digital/service) are optional"
                 WhatsAppSender.message(msg, phone_number)
                 
             else:
@@ -207,6 +212,46 @@ class WhatsAppSender:
             WhatsAppSender.message(msg, phone_number)
 
     @classmethod
+    def create_product(cls, media_id: str, info: str, phone_number: str):
+        """Creates a new product
+        Args:
+            info (str): Information provided by the user
+            phone_number (str): Phone number to send the message
+        """
+        lines = info.split("\n")
+        if (len(lines) != 6) or \
+           ("name:" not in lines[1]) or \
+           ("description:" not in lines[2]) or \
+           ("price:" not in lines[3]) or \
+           ("category:" not in lines[4]) or \
+           ("quantity:" not in lines[5]):
+            WhatsAppSender.message("Please copy the product template and edit it.", phone_number)
+        name = lines[1].split(":")[1].strip()
+        desc = lines[2].split(":")[1].strip()
+        price = lines[3].split(":")[1].strip()
+        category = lines[4].split(":")[1].strip()
+        quantity = lines[5].split(":")[1].strip()
+
+        if not price.isdigit():
+            WhatsAppSender.message("Price must be a number in NGN.", phone_number)
+            return
+        if category not in ["goods", "service", "digital"]:
+            WhatsAppSender.message("Category must be one of 'goods', 'service', 'digital'", phone_number)
+            return
+        if category == "goods" and not quantity.isdigit():
+            WhatsAppSender.message("Quantity was not set.", phone_number)
+            return
+        users = models.storage.search("User", phone=phone_number)
+        if users:
+            user = users[0]
+            product = user.create_product(name, desc, int(price), int(quantity), category)
+            product.thumbnail = media_id
+            models.storage.save()
+            WhatsAppSender.message(f"Product '{product.name}' has been added to the MarketPlace", phone_number)
+        else:
+            WhatsAppSender.message("You have not been registered", phone_number)
+
+    @classmethod
     def message(cls, message: str, phone_number: str):
         """Sends message to the phone number
         Args:
@@ -229,4 +274,5 @@ class WhatsAppSender:
         url = f"https://graph.facebook.com/{getenv('WHATSAPP_API_VERSION')}" \
               f"/{getenv('WHATSAPP_PHONE_ID')}/messages"
         
-        requests.post(url, json=json, headers=headers)
+        t = requests.post(url, json=json, headers=headers)
+        print(t.text)
